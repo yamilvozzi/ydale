02ed867e6775fee178f6d30f33353a8cfc00a1ef
yamilvozzi <yamilvozzi@gmail.com>
2026-07-24 13:22:22 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.jsx'
 import { PinProvider } from './context/PinContext.jsx'
 import PinGate from './components/PinGate.jsx'
+
+registerSW({
+  immediate: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
